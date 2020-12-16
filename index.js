3c6c107f2c653e83a566e8cfd43237c5176fdb82
Tobi <=>
2020-12-16 13:45:13 -0600

    Base template for middleware created

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    auth: require("./src/auth").default
+};
---
+++ src/auth.js
@@ -0,0 +1,7 @@
+const auth = (req, res, next) => {
+    //Add user shit here and pass next
+    //Add database functions
+    next()
+};
+
+module.exports = auth;
