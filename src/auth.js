ee0d6c1edb3456dbd680c9e7281b65f22f9a3e05
Tobi <=>
2020-12-16 15:07:33 -0600

    Middleware made and database options added

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,47 @@
-const auth = (req, res, next) => {
-    //Add user shit here and pass next
-    //Add database functions
-    next()
-};
+const database = require("@noku-app/nokubase").database
 
-module.exports = auth;
+module.exports = (opts={}) => {
+    const nokubase;
+    if (opts.database) {
+        nokubase = opts.database;
+    } else {
+        nokubase = new database(opts.config);
+    };
+    return async (req, res, next) => {
+        //Handle token authentication and user info here
+
+        if (!req.body.token) {
+            res.status(401);
+            return res.send(
+                {
+                    error: true,
+                    data: {
+                        reason: "missing_token",
+                        message: "You are missing a token in your json body"
+                    }
+                }
+            );
+        } else {
+            nokubase.userByToken(
+                req.body.token,
+                async (user) => {
+                    if (!user) {
+                        res.status(401);
+                        return res.send(
+                            {
+                                error: true,
+                                data: {
+                                    reason: "invalid_token",
+                                    message: "Your token is invalid or broken"
+                                }
+                            }
+                        );
+                    } else {
+                        req.user = user;
+                        next();
+                    }
+                }
+            );
+        };
+    };
+};
